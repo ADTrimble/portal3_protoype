292fceb564d2a1bdcbec71f41be8850affe3a9de
ADTrimble <adnan_sultan@trimble.com>
2016-11-07 09:27:07 +0200

    removed unecessary depencies in Module.ts
    removed unecessary depencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { LicenseComponent } from './license/license.component';
-import { LicenseListComponent } from './license-list/license-list.component';
-import { LicenseDetailsComponent } from './license-details/license-details.component';
 import { AssetPortfolioComponent } from './asset-portfolio/asset-portfolio.component';
 import { AssetDetailsComponent } from './asset-details/asset-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LicenseComponent,
-    LicenseListComponent,
-    LicenseDetailsComponent,
     AssetPortfolioComponent,
     AssetDetailsComponent
   ],
